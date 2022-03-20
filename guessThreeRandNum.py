ffae6cad8cbd17504714ea00e4fbebac726b0700
Afolabi Abdulsamad <fullendmaestro@gmail.com>
2022-03-20 09:02:23 +0100

    fixed implement guess random number for three numbers

--- guessThreeRandNum.py
+++ guessThreeRandNum.py
@@ -5,12 +5,15 @@ def guessThreeRandNum():
     num2 = random.randint(0, 10)
     num3 = random.randint(0, 10)
 
+    answer = { num1 + num2 + num3 }
+
     uInput1 = input('Guess the first number? ')
     uInput2 = input('Guess the second number? ')
     uInput3 = input('Guess the third number? ')
     
+    guess = { uInput1 + uInput2 + uInput3 }
+
     while True:
-        guess = int(input('Guess a number between one to ten? '))
         if guess == answer:
             print(f'Correct! The answer is {guess}')
             break
