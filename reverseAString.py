99aad7b46d888f84e463ad31d1ebb2a8849beba3
Afolabi Abdulsamad <fullendmaestro@gmail.com>
2022-04-19 10:00:36 +0100

    implement new revrsed string method

--- reverseAString.py
+++ reverseAString.py
@@ -4,3 +4,8 @@ def reverse_string_with_loop(input_string):
     for char in input_string:
         reversed_str = char + reversed_str
     return reversed_str
+
+
+def reverse_string(input_string):
+    return input_string[::-1]
+
